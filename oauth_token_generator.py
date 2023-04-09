87d37ff66fc4ec7ef9137c561532b0a29730e0c2
Rv <129611072+daddyRv@users.noreply.github.com>
2023-04-09 16:42:28 +1000

    Create oauth_token_generator.py

---
+++ oauth_token_generator.py
@@ -0,0 +1,22 @@
+import requests
+
+# Set up the OAuth token endpoint URL
+oauth_url = 'https://id.twitch.tv/oauth2/token'
+
+# Set up the OAuth token endpoint request parameters
+params = {
+    'grant_type': 'client_credentials',
+    'client_id': 'your_client_id_here',
+    'client_secret': 'your_client_secret_here',
+    'scope': 'user:read:email'
+}
+
+# Send the OAuth token endpoint request and parse the response
+response = requests.post(oauth_url, params=params)
+response_data = response.json()
+
+# Extract the access token from the response data
+access_token = response_data['access_token']
+
+# Print the access token to confirm that it was successfully generated
+print(f'Access token: {access_token}')
